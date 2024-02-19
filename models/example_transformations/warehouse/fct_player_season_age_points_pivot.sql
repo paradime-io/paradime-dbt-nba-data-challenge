57631f65c367eb4239c20579ca8795aa91fcc520
Gautam Doulani <gautam.doulani@airbnb.com>
2024-02-19 17:32:50 +0000

    pivot

--- models/example_transformations/warehouse/fct_player_season_age_points_pivot.sql
+++ models/example_transformations/warehouse/fct_player_season_age_points_pivot.sql
@@ -1,9 +1,9 @@
   SELECT 
     age,
     {{ dbt_utils.pivot('season',
-                       values=dbt_utils.get_column_values(ref('fct_player_season_age_points'), 'season'),
+                       values=dbt_utils.get_column_values(ref('fct_player_season_age_points'), 'season', order_by='season'),
                        agg='sum',
-                       then_value='points'
+                       then_value='season_points_percentage'
                        )
                      }}
   FROM {{ ref('fct_player_season_age_points') }}
