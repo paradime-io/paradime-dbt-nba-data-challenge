9108b3438a50e5fcaf25c75c4de5f7900d6ecabe
Kevin Schulman <kevinhannon95@gmail.com>
2024-03-05 22:06:02 +0000

    amended international signing model

--- models/example_transformations/warehouse/fct_international_signings.sql
+++ models/example_transformations/warehouse/fct_international_signings.sql
@@ -1,6 +1,9 @@
-with final as (select count(player_id) as num_player_debuts, country, first_year_played
+with playerdebuts as (select count(player_id) as num_player_debuts, country, first_year_played
 from {{ ref('stg_common_player_info') }}
 group by country, first_year_played
 order by first_year_played asc)
 --the above CTE counts the number of players from each country who made their NBA debut in a given year
+, final as (select country, first_year_played, 
+sum(num_player_debuts) over (partition by country order by first_year_played) as totalplayers from playerdebuts)
+
 select * from final
