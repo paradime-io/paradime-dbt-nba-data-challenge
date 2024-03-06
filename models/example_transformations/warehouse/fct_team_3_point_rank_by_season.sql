528e3122af076c94dec300a5534e06506d6dc9c2
Kevin Schulman <kevinhannon95@gmail.com>
2024-03-06 19:13:17 +0000

    revised 3 point rank model and yml

--- models/example_transformations/warehouse/fct_team_3_point_rank_by_season.sql
+++ models/example_transformations/warehouse/fct_team_3_point_rank_by_season.sql
@@ -8,7 +8,7 @@ order by season asc)
 dense_rank() over (partition by season order by three_pointers_attempted desc) as three_point_rank from team_three_pointers)
 --the above CTE produces a team ranking and quantity of three pointers attempted per team per season
 , final as (select team_name, season, three_point_rank, three_pointers_attempted from team_rank 
-where three_point_rank between 1 and 10 group by team_name, season, three_point_rank, three_pointers_attempted 
+where three_point_rank between 1 and 5 group by team_name, season, three_point_rank, three_pointers_attempted 
 order by season asc, three_point_rank asc)
---the above CTE produces the top ten ranked teams per season by three pointers attempted and the number of three pointers attempted by each team
+--the above CTE produces the top five ranked teams per season by three pointers attempted and the number of three pointers attempted by each team
 select * from final
