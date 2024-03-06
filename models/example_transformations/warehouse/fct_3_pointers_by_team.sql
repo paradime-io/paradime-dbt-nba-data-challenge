5f8a62753843d7a4e548d01b2f1c13c748ab4df3
Kevin Schulman <kevinhannon95@gmail.com>
2024-03-06 16:22:03 +0000

    amended 3 point model and revised yml

--- models/example_transformations/warehouse/fct_3_pointers_by_team.sql
+++
@@ -1,7 +0,0 @@
-with final as (select team_name, season, field_goals_attempted, field_goals_made, three_pointers_attempted, three_pointers_made
-from {{ ref('stg_team_stats_by_season') }}
-where field_goals_attempted > 0 --some season have zero field goals attempted so this filters those seasons out
-group by team_name, season, field_goals_attempted, field_goals_made, three_pointers_attempted, three_pointers_made
-order by season asc)
---the above CTE selects the number of FGs and 3Ps attempted and made per team per season
-select * from final
---
+++ models/example_transformations/warehouse/fct_team_3_point_rank_by_season.sql
@@ -0,0 +1,14 @@
+with team_three_pointers as (select team_name, season, three_pointers_attempted
+from {{ ref('stg_team_stats_by_season') }}
+where three_pointers_attempted > 0
+group by team_name, season, three_pointers_attempted
+order by season asc)
+
+, team_rank as (select team_name, season, three_pointers_attempted, 
+dense_rank() over (partition by season order by three_pointers_attempted desc) as three_point_rank from team_three_pointers)
+
+, final as (select team_name, season, three_point_rank, three_pointers_attempted from team_rank 
+where three_point_rank between 1 and 10 group by team_name, season, three_point_rank, three_pointers_attempted 
+order by season asc, three_point_rank asc)
+
+select * from final
