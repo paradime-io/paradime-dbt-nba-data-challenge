1ba1240c79c1f4375a47dc159507d5a9b8b63264
Parker Rogers <parker@paradimehq.com>
2024-03-06 17:31:30 +0000

    This is done.

--- models/example_transformations/intermediate/player_game_logs_agg_example.sql
+++ models/example_transformations/intermediate/player_game_logs_agg_example.sql
@@ -7,6 +7,7 @@ WITH player_game_logs_agg AS (
         game_type,
         -- Total field goals made by the player
         SUM(field_goals_made) AS field_goals_made,
+        
         -- Total field goals attempted by the player
         SUM(field_goals_attempted) AS field_goals_attempted,
         -- Field goal percentage (made attempts over total attempts)
