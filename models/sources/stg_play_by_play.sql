cf7f33a5454b40ad96dce0c37807544f9939f37b
Anthony Fancher <tony.fancher@securly.com>
2024-03-09 09:12:47 +0000

    update with play by play

---
+++ models/sources/stg_play_by_play.sql
@@ -0,0 +1,40 @@
+WITH source AS (
+    SELECT
+        *
+    FROM
+        {{ source('NBA_RAW', 'PLAYBYPLAY') }}
+),
+
+
+renamed AS (
+    SELECT
+        gameId as game_id,
+        actionId as action_id,
+        clock as game_clock,
+        period,
+        teamId as team_id,
+        teamTricode as team_abbreviation,
+        personId as player_id,
+        playerName as player_family_name,
+        playerNameI as player_name,
+        xLegacy as shot_x_coordinate,
+        yLegacy as shot_y_coordinate,
+        shotDistance as shot_distance,
+        shotResult as shot_result,
+        isFieldGoal::BOOLEAN as is_field_goal,
+        scoreHome as score_home,
+        scoreAway as score_away,
+        pointsTotal as score_total,
+        location as game_location,
+        description as play_description,
+        actionType as action_type,
+        subType as action_sub_type,
+        videoAvailable::BOOLEAN as video_available
+    FROM
+        source
+)
+
+SELECT
+    *
+FROM
+    renamed
